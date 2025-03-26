efa9dc54b5ca3927e84800dc80917b9f05077047
LianXia233 <49828512+LianXia233@users.noreply.github.com>
2025-03-27 01:01:56 +0800

    Update filogic.mk

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -134,14 +134,22 @@ endef
 TARGET_DEVICES += abt_asr3000
 
 # 定义 ZX7981PM 设备信息
-define Device/zx7981pm
-  DEVICE_VENDOR := ZX
-  DEVICE_MODEL := 7981PM
-  DEVICE_DTS := zx7981pm
-  IMAGE_SIZE := 64m
-  DEVICE_PACKAGES := kmod-mt7981-firmware kmod-mt7915-firmware
+define Device/ZX7981PM
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := ZX7981PM
+  DEVICE_DTS := ZX7981PM
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES := ZX7981PM
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += zx7981pm
+TARGET_DEVICES += ZX7981PM
 
 define Device/acelink_ew-7886cax
   DEVICE_VENDOR := Acelink
