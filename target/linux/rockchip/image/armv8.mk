4fc4821f4afb7167a40d5057e18dfa9c3b08f72b
LianXia233 <49828512+LianXia233@users.noreply.github.com>
2025-05-05 19:46:19 +0800

    Update armv8.mk

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -10,6 +10,16 @@ define Device/IfnameMigration
   DEVICE_COMPAT_MESSAGE := Network interface names have been changed
 endef
 
+define Device/hinlink_opc-h29k
+  DEVICE_VENDOR := HINLINK
+  DEVICE_MODEL := OPC-H29K
+  SOC := rk3528
+  UBOOT_DEVICE_NAME := generic-rk3528
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script rk3528 | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-aic8800s kmod-fb-tft-st7789v wpad-openssl -urngd
+endef
+TARGET_DEVICES += hinlink_opc-h29k
+
 define Device/ariaboard_photonicat
   DEVICE_VENDOR := Ariaboard
   DEVICE_MODEL := Photonicat
